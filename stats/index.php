09fdf11bf0328198f818766486dfa1951e8a731e
Jacek Dominiak <doj@ptpbs.com>
2011-12-22 19:26:16 +0100

    added website as well

---
+++ stats/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<link href='http://fonts.googleapis.com/css?family=Duru+Sans' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="style.css" />
+<title>Bravo Statistics</title>
+</head>
+<body>
+<div id="content">
+<h1>Statistics for <?php echo date("l"); ?></h1>
+<img src="load.png" />
+<img src="hdd.png" />
+<img src="eth.png" />
+</div>
+</body>
+</html>
